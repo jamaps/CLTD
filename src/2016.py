9e5741c9704767be5ecf26a63d28c4bb98739c47
jamaps <jeffallen89@gmail.com>
2022-06-08 08:59:38 -0400

    setup db config, connect - also gitignore

---
+++ src/2016.py
@@ -0,0 +1,16 @@
+import psycopg2
+from config import config as conf
+
+# connect to database
+connection = psycopg2.connect(
+	"""host='{host}' port='{port}' dbname='{dbname}'
+		user='{user}' password='{password}'
+	""".format(
+		host = conf['pg']['host'],
+		port = conf['pg']['port'],
+		dbname = conf['pg']['database'],
+		user = conf['pg']['user'],
+		password = conf['pg']['password']
+	)
+)
+connection.autocommit = True
