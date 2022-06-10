1dc71e2304ff8819b1ed240619fe006acbac3bff
jamaps <jeffallen89@gmail.com>
2022-06-10 08:54:04 -0400

    add back in blocks that were never clipped

--- src/2016poparea.sql
+++ src/2016poparea.sql
@@ -24,7 +24,6 @@ CREATE TABLE x_source_blocks AS (
     geom 
     FROM in_2016_cbf_db
     WHERE ctuid IS NOT NULL
-    AND cmauid = '535'
     ORDER BY ctuid);
 
 DROP INDEX IF EXISTS x_source_blocks_geom_idx;
@@ -42,6 +41,22 @@ CREATE TABLE x_source_blocks_clipped AS (
     ON ST_Intersects(ST_MakeValid(x_source_blocks.geom),ST_MakeValid(c.geom))
     WHERE x_source_blocks.geom && c.geom
     GROUP BY ctuid, dbuid
+);
+
+DROP TABLE IF EXISTS x_source_blocks_clipped_all;
+CREATE TABLE x_source_blocks_clipped_all AS (
+    (SELECT * FROM x_source_blocks_clipped)
+    UNION ALL
+    (WITH J AS (SELECT 
+        in_2016_cbf_db.dbuid,
+        x_source_blocks_clipped.dbuid AS clipped_db,
+        in_2016_cbf_db.ctuid,
+        in_2016_cbf_db.geom 
+        FROM in_2016_cbf_db
+        LEFT JOIN x_source_blocks_clipped ON
+        in_2016_cbf_db.dbuid = x_source_blocks_clipped.dbuid
+        WHERE in_2016_cbf_db.ctuid IS NOT NULL)
+    SELECT dbuid, ctuid, geom FROM J WHERE clipped_db IS NULL)
 );
 
 DROP TABLE IF EXISTS x_source_blocks_clipped_ready;
@@ -73,16 +88,16 @@ db_ct_op AS
     ON db_pop_ctuid.ctuid = pop_ct_2016.ctuid
     ORDER BY ctuid)
 SELECT
-x_source_blocks_clipped.ctuid,
-x_source_blocks_clipped.dbuid,
-ST_AREA(x_source_blocks_clipped.geom::geography)::integer as db_area,
+x_source_blocks_clipped_all.ctuid,
+x_source_blocks_clipped_all.dbuid,
+ST_AREA(x_source_blocks_clipped_all.geom::geography)::bigint as db_area,
 db_ct_op.db_pop,
 db_ct_op.db_dwe,
 db_ct_op.ct_pop,
 db_ct_op.ct_dwe,
-x_source_blocks_clipped.geom
-FROM x_source_blocks_clipped LEFT JOIN db_ct_op
-ON x_source_blocks_clipped.dbuid = db_ct_op.dbuid
+x_source_blocks_clipped_all.geom
+FROM x_source_blocks_clipped_all LEFT JOIN db_ct_op
+ON x_source_blocks_clipped_all.dbuid = db_ct_op.dbuid
 );
 
 DROP INDEX IF EXISTS x_source_blocks_clipped_ready_geom_idx;
@@ -119,8 +134,11 @@ CREATE TABLE x_ct_2016_2021 AS (
     ORDER BY source_ctuid, target_ctuid
 );
 
+SELECT ctuid FROM in_2021_dbf_ct WHERE ctuid NOT IN (SELECT target_ctuid FROM x_ct_2016_2021)
+ORDER BY ctuid;
+
 -- now, auto?
 --- anything not in source and target, add in
 --- remove slivers
 --- re-balance to 1
---- make sure have both pop and dwe
+--- make sure have both pop and dwe 
