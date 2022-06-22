5ebeaea86f6b9d8f77f49907d03397bb62b33f6d
jamaps <jeffallen89@gmail.com>
2022-06-22 09:01:55 -0400

    2006 finish running

--- src/2006poparea.sql
+++ src/2006poparea.sql
@@ -53,14 +53,14 @@ CREATE TABLE x_source_blocks_clipped_all AS (
     (SELECT * FROM x_source_blocks_clipped)
     UNION ALL
     (WITH J AS (SELECT 
-        in_2011_cbf_db.dbuid,
+        in_2006_cbf_db.dbuid,
         x_source_blocks_clipped.dbuid AS clipped_db,
-        in_2011_cbf_db.ctuid,
-        in_2011_cbf_db.geom 
-        FROM in_2011_cbf_db
+        in_2006_cbf_db.ctuid,
+        in_2006_cbf_db.geom 
+        FROM in_2006_cbf_db
         LEFT JOIN x_source_blocks_clipped ON
-        in_2011_cbf_db.dbuid = x_source_blocks_clipped.dbuid
-        WHERE in_2011_cbf_db.ctuid IS NOT NULL)
+        in_2006_cbf_db.dbuid = x_source_blocks_clipped.dbuid
+        WHERE in_2006_cbf_db.ctuid IS NOT NULL)
     SELECT dbuid, ctuid, geom FROM J WHERE clipped_db IS NULL)
 );
 
@@ -72,16 +72,16 @@ CREATE TABLE x_source_blocks_clipped_ready AS (
 WITH db_pop AS 
     (SELECT 
     (CASE WHEN dbpop~E'^\\d+$' THEN dbpop::integer ELSE 0 END) AS db_pop,
-    (CASE WHEN dbtdwell~E'^\\d+$' THEN dbtdwell::integer ELSE 0 END) AS db_dwe,
+    (CASE WHEN dbdwell~E'^\\d+$' THEN dbdwell::integer ELSE 0 END) AS db_dwe,
     dbuid as dbuid
-    FROM in_2011_gaf_pt),
+    FROM in_2006_gaf_pt),
 db_pop_ctuid AS 
     (SELECT
     C.ctuid,
     C.dbuid,
     db_pop.db_pop AS db_pop,
     db_pop.db_dwe AS db_dwe
-    FROM db_pop LEFT JOIN (SELECT dbuid, ctuid FROM in_2011_cbf_db) AS C
+    FROM db_pop LEFT JOIN (SELECT dbuid, ctuid FROM in_2006_cbf_db) AS C
     ON db_pop.dbuid = C.dbuid
     WHERE C.ctuid IS NOT NULL),
 db_ct_op AS 
@@ -90,10 +90,10 @@ db_ct_op AS
     db_pop_ctuid.dbuid,
     db_pop_ctuid.db_pop,
     db_pop_ctuid.db_dwe,
-    pop_ct_2011.ct_pop,
-    pop_ct_2011.ct_dwe
-    FROM db_pop_ctuid LEFT JOIN pop_ct_2011 
-    ON db_pop_ctuid.ctuid = pop_ct_2011.ctuid
+    pop_ct_2006.ct_pop,
+    pop_ct_2006.ct_dwe
+    FROM db_pop_ctuid LEFT JOIN pop_ct_2006 
+    ON db_pop_ctuid.ctuid = pop_ct_2006.ctuid
     ORDER BY ctuid)
 SELECT
 x_source_blocks_clipped_all.ctuid,
@@ -138,8 +138,8 @@ CREATE TABLE x_source_target AS (
     WHERE x_source_blocks_clipped_ready.geom && in_2021_dbf_ct.geom
 );
 
-DROP TABLE IF EXISTS x_ct_2011_2021;
-CREATE TABLE x_ct_2011_2021 AS (
+DROP TABLE IF EXISTS x_ct_2006_2021;
+CREATE TABLE x_ct_2006_2021 AS (
     ((SELECT
     source_ctuid,
     target_ctuid,
@@ -161,7 +161,7 @@ CREATE TABLE x_ct_2011_2021 AS (
             ST_MakeValid(f.geom),
             (
                 SELECT ST_Union(ST_MakeValid(l.geom))
-                FROM in_2011_cbf_ct l 
+                FROM in_2006_cbf_ct l 
                 WHERE ST_Intersects(ST_MakeValid(l.geom),ST_MakeValid(l.geom))
             )
         ) as geom
@@ -197,7 +197,7 @@ CREATE TABLE x_ct_2011_2021 AS (
                 WHERE ST_Intersects(ST_MakeValid(l.geom),ST_MakeValid(l.geom))
             )
         ) as geom
-    FROM in_2011_cbf_ct f),
+    FROM in_2006_cbf_ct f),
     x_diff_target_area AS (
     SELECT
     source_ctuid,
@@ -215,26 +215,26 @@ CREATE TABLE x_ct_2011_2021 AS (
 );
 
     
--- join to the target tracts - this case to 2016
+-- join to the target tracts - this case to 2011
 DROP TABLE IF EXISTS x_source_target;
 CREATE TABLE x_source_target AS (
     SELECT
     x_source_blocks_clipped_ready.dbuid AS source_dbuid,
     x_source_blocks_clipped_ready.ctuid AS source_ctuid,
-    in_2016_dbf_ct.ctuid AS target_ctuid,
+    in_2011_dbf_ct.ctuid AS target_ctuid,
     x_source_blocks_clipped_ready.db_pop,
     x_source_blocks_clipped_ready.db_dwe,
     x_source_blocks_clipped_ready.ct_pop + 0.001 AS ct_pop,
     x_source_blocks_clipped_ready.ct_dwe + 0.001 AS ct_dwe,
     x_source_blocks_clipped_ready.db_area + 0.001 AS db_area,
-    ST_Intersection(ST_MakeValid(x_source_blocks_clipped_ready.geom),ST_MakeValid(in_2016_dbf_ct.geom)) AS geom
-    FROM x_source_blocks_clipped_ready LEFT JOIN in_2016_dbf_ct
-    ON ST_Intersects(ST_MakeValid(x_source_blocks_clipped_ready.geom),ST_MakeValid(in_2016_dbf_ct.geom))
-    WHERE x_source_blocks_clipped_ready.geom && in_2016_dbf_ct.geom
+    ST_Intersection(ST_MakeValid(x_source_blocks_clipped_ready.geom),ST_MakeValid(in_2011_dbf_ct.geom)) AS geom
+    FROM x_source_blocks_clipped_ready LEFT JOIN in_2011_dbf_ct
+    ON ST_Intersects(ST_MakeValid(x_source_blocks_clipped_ready.geom),ST_MakeValid(in_2011_dbf_ct.geom))
+    WHERE x_source_blocks_clipped_ready.geom && in_2011_dbf_ct.geom
 );
 
-DROP TABLE IF EXISTS x_ct_2011_2016;
-CREATE TABLE x_ct_2011_2016 AS (
+DROP TABLE IF EXISTS x_ct_2006_2011;
+CREATE TABLE x_ct_2006_2011 AS (
     ((SELECT
     source_ctuid,
     target_ctuid,
@@ -256,11 +256,11 @@ CREATE TABLE x_ct_2011_2016 AS (
             ST_MakeValid(f.geom),
             (
                 SELECT ST_Union(ST_MakeValid(l.geom))
-                FROM in_2011_cbf_ct l 
+                FROM in_2006_cbf_ct l 
                 WHERE ST_Intersects(ST_MakeValid(l.geom),ST_MakeValid(l.geom))
             )
         ) as geom
-    FROM in_2016_cbf_ct f),
+    FROM in_2011_cbf_ct f),
     x_diff_target_area AS (
     SELECT
     source_ctuid,
@@ -288,11 +288,11 @@ CREATE TABLE x_ct_2011_2016 AS (
             ST_MakeValid(f.geom),
             (
                 SELECT ST_Union(ST_MakeValid(l.geom))
-                FROM in_2016_cbf_ct l 
+                FROM in_2011_cbf_ct l 
                 WHERE ST_Intersects(ST_MakeValid(l.geom),ST_MakeValid(l.geom))
             )
         ) as geom
-    FROM in_2011_cbf_ct f),
+    FROM in_2006_cbf_ct f),
     x_diff_target_area AS (
     SELECT
     source_ctuid,
