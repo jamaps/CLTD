487041434104b91bd0307a7a39988ec69e3c771e
jamaps <jeffallen89@gmail.com>
2022-07-18 18:02:14 -0400

    create combined ea and bf pt file

--- src/90s_translate/clean1996.py
+++ src/90s_translate/clean1996.py
@@ -33,6 +33,7 @@ dfd = dfd.merge(df.groupby(['ctuid'])['pop_count'].sum().reset_index(), how="lef
 dfctea = dfd[(dfd["ct_pop"] > dfd["pop_count"]) | (dfd["pop_count"].isna())]["ctuid"].tolist()
 df = df[~df["ctuid"].isin(dfctea)]
 
+
 # select EAs that are still needed
 
 sql = """
@@ -54,6 +55,38 @@ ea = gpd.read_postgis(sql, con)
 ea = ea[ea["ctuid"].isin(dfctea)]
 
 
+# generate random points by DA, 1 per 25 population
+
+eapts = []
+for index, row in ea.iterrows():
+	try:
+		points = []
+		number = int(row["ea_pop"] / 25) + 1
+		min_x, min_y, max_x, max_y = row["geom"].bounds
+		i = 0
+		while i < number:
+			point = Point(random.uniform(min_x, max_x), random.uniform(min_y, max_y))
+			if row["geom"].contains(point):
+				points.append([point.x,point.y])
+				i += 1
+		for p in points:
+			pop = 0.0001 + row["ea_pop"] / number
+			dwe = 0.0001 + row["ea_dwe"] / number
+			eapts.append([row["eauid"],row["ctuid"],pop,dwe,p[0],p[1]])
+	except:
+		print(index,row)
+
+eapts = pd.DataFrame(eapts, columns = ['eauid', 'ctuid', 'pop_count', 'dwe_count', 'long', 'lat'])
+eapts["cmaca_code"] = eapts["ctuid"].str[:3]
 
 
-tr = pd.read_csv("translate_table.csv", dtype={'cma': 'str'} )
+# combine the block and ea points
+
+df["pt_type"] = "bf"
+eapts["pt_type"] = "ea_rand"
+
+df = pd.concat([df, eapts], axis=0)
+
+df.to_csv("data/pts_1996.csv")
+
+# tr = pd.read_csv("translate_table.csv", dtype={'cma': 'str'} )
