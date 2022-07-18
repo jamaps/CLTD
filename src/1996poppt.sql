3ad12388deabd0faba617a48283e6269e2892683
jamaps <jeffallen89@gmail.com>
2022-07-18 13:31:24 -0400

    get pop for 1996 in db from grouping EAs

--- src/1996poppt.sql
+++ src/1996poppt.sql
@@ -2,3 +2,23 @@
 DROP INDEX IF EXISTS in_1996_mypoints_all_geom_idx; 
 CREATE INDEX in_1996_mypoints_all_geom_idx ON in_1996_mypoints_all USING GIST (geom);
 
+
+-- create CT population table
+DROP TABLE IF EXISTS pop_ct_1996;
+CREATE TABLE pop_ct_1996 AS (
+WITH source_ea_pop AS 
+    (SELECT 
+    ea_pop::integer AS ea_pop,
+    cc_pri_dwe::integer AS ea_dwe,
+    eauid as eauid,
+    cacode || ct_name as ctuid
+    FROM in_1996_gaf_pt
+    WHERE ct_name IS NOT NULL)
+SELECT 
+ctuid,
+sum(ea_pop) AS ct_pop,
+sum(ea_dwe) AS ct_dwe
+FROM source_ea_pop 
+GROUP BY ctuid
+ORDER BY ctuid
+);
