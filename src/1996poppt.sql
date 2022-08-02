0ff7fb906f784f1e7a2b1779520115622ddeec46
jamaps <jeffallen89@gmail.com>
2022-08-02 16:26:12 -0400

    create iteration 1 of 1996 weights

--- src/1996poppt.sql
+++ src/1996poppt.sql
@@ -22,3 +22,37 @@ FROM source_ea_pop
 GROUP BY ctuid
 ORDER BY ctuid
 );
+
+
+-- join target to block face points, then group by to create weights
+WITH blocks_with_target AS (
+	SELECT 
+	in_1996_mypoints_all.ctuid AS source_ctuid,
+	in_2001_dbf_ct.ctuid AS target_ctuid,
+	in_1996_mypoints_all.pop_count + 0.001 AS bf_pop,
+	in_1996_mypoints_all.dwe_count + 0.001 AS bf_dwe
+	FROM
+	in_1996_mypoints_all
+	LEFT JOIN 
+	in_2001_dbf_ct ON ST_Intersects(in_1996_mypoints_all.geom, in_2001_dbf_ct.geom)
+	WHERE in_1996_mypoints_all.geom && in_2001_dbf_ct.geom	
+),
+ct_sums AS (
+	SELECT 
+	source_ctuid AS source_ctuid,
+	SUM(bf_pop) AS source_ct_pop,
+	SUM(bf_dwe) AS source_ct_dwe
+	FROM blocks_with_target
+	GROUP BY source_ctuid
+	ORDER BY source_ctuid
+)
+SELECT
+blocks_with_target.source_ctuid,
+blocks_with_target.target_ctuid,
+SUM(blocks_with_target.bf_pop / ct_sums.source_ct_pop) AS w_pop,
+SUM(blocks_with_target.bf_dwe / ct_sums.source_ct_dwe) AS w_dwe
+FROM
+blocks_with_target LEFT JOIN ct_sums
+ON blocks_with_target.source_ctuid = ct_sums.source_ctuid
+GROUP BY blocks_with_target.source_ctuid, blocks_with_target.target_ctuid
+ORDER BY blocks_with_target.source_ctuid, blocks_with_target.target_ctuid;
