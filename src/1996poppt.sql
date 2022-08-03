5239ae25042c00295dd35d36d58a1aab188b76b7
jamaps <jeffallen89@gmail.com>
2022-08-03 10:17:39 -0400

    1996 include outside CTs, update spatial indices

--- src/1996poppt.sql
+++ src/1996poppt.sql
@@ -2,7 +2,6 @@
 DROP INDEX IF EXISTS in_1996_mypoints_all_geom_idx; 
 CREATE INDEX in_1996_mypoints_all_geom_idx ON in_1996_mypoints_all USING GIST (geom);
 
-
 -- create CT population table
 DROP TABLE IF EXISTS pop_ct_1996;
 CREATE TABLE pop_ct_1996 AS (
@@ -25,6 +24,8 @@ ORDER BY ctuid
 
 
 -- join target to block face points, then group by to create weights
+DROP TABLE IF EXISTS x_ct_1996_2021;
+CREATE TABLE x_ct_1996_2021 AS (
 WITH blocks_with_target AS (
 	SELECT 
 	in_1996_mypoints_all.ctuid AS source_ctuid,
@@ -45,8 +46,73 @@ ct_sums AS (
 	FROM blocks_with_target
 	GROUP BY source_ctuid
 	ORDER BY source_ctuid
+), 
+x_diff_target AS (
+	SELECT 	
+		'-1' AS source_ctuid,
+		ctuid AS target_ctuid,
+		-1 AS w_pop,
+		-1 AS w_dwe,
+			ST_Difference(
+			ST_MakeValid(f.geom),
+			(
+				SELECT ST_Union(ST_MakeValid(l.geom))
+				FROM in_1996_cbf_ct_moved l 
+				WHERE ST_Intersects(ST_MakeValid(l.geom),ST_MakeValid(l.geom))
+			)
+		) as geom
+	FROM in_2001_cbf_ct f),
+x_diff_target_area AS (
+	SELECT
+	source_ctuid,
+	target_ctuid,
+	w_pop,
+	w_dwe,
+	ST_Area(geom::geography) AS area
+	FROM x_diff_target),
+x_diff_source AS (
+	SELECT 
+		ctuid AS source_ctuid,
+		'-1' AS target_ctuid,
+		0 AS w_pop,
+		0 AS w_dwe,
+			ST_Difference(
+			ST_MakeValid(f.geom),
+			(
+				SELECT ST_Union(ST_MakeValid(l.geom))
+				FROM in_2001_cbf_ct l 
+				WHERE ST_Intersects(ST_MakeValid(l.geom),ST_MakeValid(l.geom))
+			)
+		) as geom
+	FROM in_1996_cbf_ct_moved f),
+x_diff_source_area AS (
+	SELECT
+	source_ctuid,
+	target_ctuid,
+	w_pop,
+	w_dwe,
+	ST_Area(geom::geography) AS area
+	FROM x_diff_source),
+x_diff AS (
+	SELECT 
+	source_ctuid,
+	target_ctuid,
+	w_pop,
+	w_dwe
+	FROM x_diff_source_area WHERE area > 1000000
+
+	UNION
+	
+	SELECT 
+	source_ctuid,
+	target_ctuid,
+	w_pop,
+	w_dwe
+	FROM x_diff_target_area WHERE area > 1000000
 )
-SELECT
+(SELECT * FROM x_diff)
+UNION
+(SELECT
 blocks_with_target.source_ctuid,
 blocks_with_target.target_ctuid,
 SUM(blocks_with_target.bf_pop / ct_sums.source_ct_pop) AS w_pop,
@@ -55,4 +121,107 @@ FROM
 blocks_with_target LEFT JOIN ct_sums
 ON blocks_with_target.source_ctuid = ct_sums.source_ctuid
 GROUP BY blocks_with_target.source_ctuid, blocks_with_target.target_ctuid
-ORDER BY blocks_with_target.source_ctuid, blocks_with_target.target_ctuid;
+ORDER BY blocks_with_target.source_ctuid, blocks_with_target.target_ctuid)
+);
+
+
+-- join target to block face points, then group by to create weights
+DROP TABLE IF EXISTS x_ct_1996_2021;
+CREATE TABLE x_ct_1996_2021 AS (
+WITH blocks_with_target AS (
+	SELECT 
+	in_1996_mypoints_all.ctuid AS source_ctuid,
+	in_2021_dbf_ct.ctuid AS target_ctuid,
+	in_1996_mypoints_all.pop_count + 0.001 AS bf_pop,
+	in_1996_mypoints_all.dwe_count + 0.001 AS bf_dwe
+	FROM
+	in_1996_mypoints_all
+	LEFT JOIN 
+	in_2021_dbf_ct ON ST_Intersects(in_1996_mypoints_all.geom, in_2021_dbf_ct.geom)
+	WHERE in_1996_mypoints_all.geom && in_2021_dbf_ct.geom	
+),
+ct_sums AS (
+	SELECT 
+	source_ctuid AS source_ctuid,
+	SUM(bf_pop) AS source_ct_pop,
+	SUM(bf_dwe) AS source_ct_dwe
+	FROM blocks_with_target
+	GROUP BY source_ctuid
+	ORDER BY source_ctuid
+), 
+x_diff_target AS (
+	SELECT 	
+		'-1' AS source_ctuid,
+		ctuid AS target_ctuid,
+		-1 AS w_pop,
+		-1 AS w_dwe,
+			ST_Difference(
+			ST_MakeValid(f.geom),
+			(
+				SELECT ST_Union(ST_MakeValid(l.geom))
+				FROM in_1996_cbf_ct_moved l 
+				WHERE ST_Intersects(ST_MakeValid(l.geom),ST_MakeValid(l.geom))
+			)
+		) as geom
+	FROM in_2021_cbf_ct f),
+x_diff_target_area AS (
+	SELECT
+	source_ctuid,
+	target_ctuid,
+	w_pop,
+	w_dwe,
+	ST_Area(geom::geography) AS area
+	FROM x_diff_target),
+x_diff_source AS (
+	SELECT 
+		ctuid AS source_ctuid,
+		'-1' AS target_ctuid,
+		0 AS w_pop,
+		0 AS w_dwe,
+			ST_Difference(
+			ST_MakeValid(f.geom),
+			(
+				SELECT ST_Union(ST_MakeValid(l.geom))
+				FROM in_2021_cbf_ct l 
+				WHERE ST_Intersects(ST_MakeValid(l.geom),ST_MakeValid(l.geom))
+			)
+		) as geom
+	FROM in_1996_cbf_ct_moved f),
+x_diff_source_area AS (
+	SELECT
+	source_ctuid,
+	target_ctuid,
+	w_pop,
+	w_dwe,
+	ST_Area(geom::geography) AS area
+	FROM x_diff_source),
+x_diff AS (
+	SELECT 
+	source_ctuid,
+	target_ctuid,
+	w_pop,
+	w_dwe
+	FROM x_diff_source_area WHERE area > 1000000
+
+	UNION
+	
+	SELECT 
+	source_ctuid,
+	target_ctuid,
+	w_pop,
+	w_dwe
+	FROM x_diff_target_area WHERE area > 1000000
+)
+(SELECT * FROM x_diff)
+UNION
+(SELECT
+blocks_with_target.source_ctuid,
+blocks_with_target.target_ctuid,
+SUM(blocks_with_target.bf_pop / ct_sums.source_ct_pop) AS w_pop,
+SUM(blocks_with_target.bf_dwe / ct_sums.source_ct_dwe) AS w_dwe
+FROM
+blocks_with_target LEFT JOIN ct_sums
+ON blocks_with_target.source_ctuid = ct_sums.source_ctuid
+GROUP BY blocks_with_target.source_ctuid, blocks_with_target.target_ctuid
+ORDER BY blocks_with_target.source_ctuid, blocks_with_target.target_ctuid)
+);
--- src/validate_update.py
+++ src/validate_update.py
@@ -191,14 +191,15 @@ def update_crosswalk(crosswalk_table, source, target, weights):
 		result = cursor.fetchone();
 		print(result)
 
+update_crosswalk("ct_1996_2001", "in_1996_dbf_ct", "in_2001_cbf_ct", ["pop", "dwe"])
 
 # update_crosswalk("ct_2001_2006", "in_2001_cbf_ct", "in_2006_cbf_ct", ["pop", "dwe"])
 # update_crosswalk("ct_2001_2021", "in_2001_cbf_ct", "in_2021_cbf_ct", ["pop", "dwe"])
 
-update_crosswalk("ct_2006_2011", "in_2006_cbf_ct", "in_2011_cbf_ct", ["pop", "dwe"])
-update_crosswalk("ct_2006_2021", "in_2006_cbf_ct", "in_2021_cbf_ct", ["pop", "dwe"])
+# update_crosswalk("ct_2006_2011", "in_2006_cbf_ct", "in_2011_cbf_ct", ["pop", "dwe"])
+# update_crosswalk("ct_2006_2021", "in_2006_cbf_ct", "in_2021_cbf_ct", ["pop", "dwe"])
 
-update_crosswalk("ct_2011_2016", "in_2011_cbf_ct", "in_2016_cbf_ct", ["pop", "dwe"])
-update_crosswalk("ct_2011_2021", "in_2011_cbf_ct", "in_2021_cbf_ct", ["pop", "dwe"])
+# update_crosswalk("ct_2011_2016", "in_2011_cbf_ct", "in_2016_cbf_ct", ["pop", "dwe"])
+# update_crosswalk("ct_2011_2021", "in_2011_cbf_ct", "in_2021_cbf_ct", ["pop", "dwe"])
 
-update_crosswalk("ct_2016_2021", "in_2016_cbf_ct", "in_2021_cbf_ct", ["pop", "dwe"])
+# update_crosswalk("ct_2016_2021", "in_2016_cbf_ct", "in_2021_cbf_ct", ["pop", "dwe"])
