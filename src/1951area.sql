8e7a33917b81031fbd985dda6b7a32e346077fe0
Jeff Allen <jeffallen89@gmail.com>
2022-08-29 11:34:21 -0400

    union 51 56 ct

--- src/1951area.sql
+++ src/1951area.sql
@@ -3,7 +3,7 @@ DROP INDEX IF EXISTS in_1951_ct_geom_idx;
 CREATE INDEX in_1951_ct_geom_idx ON in_1951_ct USING GIST (geom);
 
 
--- clip the source CT by the built up land layer, then union out any completly clipped away
+-- clip the source CT by the built up land layer, then union out any completly clipped away, also compute area
 DROP TABLE IF EXISTS x_source_ct_clipped;
 CREATE TABLE x_source_ct_clipped AS (
     SELECT
@@ -17,14 +17,37 @@ CREATE TABLE x_source_ct_clipped AS (
 
 DROP TABLE IF EXISTS x_source_ct_clipped_all;
 CREATE TABLE x_source_ct_clipped_all AS (
-    (SELECT * FROM x_source_ct_clipped)
+    (SELECT
+	ctuid,
+	ST_AREA(x_source_ct_clipped.geom::geography)::bigint as ct_area,
+	geom
+	FROM x_source_ct_clipped)
     UNION ALL
     (SELECT 
 	in_1951_ct.geosid AS ctuid,
+	ST_AREA(in_1951_ct.geom::geography)::bigint as ct_area,
 	in_1951_ct.geom AS geom
 	FROM in_1951_ct
 	WHERE in_1951_ct.geosid NOT IN (SELECT ctuid FROM x_source_ct_clipped)
 	)
 );
 
+DROP INDEX IF EXISTS x_source_ct_clipped_all_geom_idx;
+CREATE INDEX x_source_ct_clipped_all_geom_idx
+ON x_source_ct_clipped_all
+USING GIST (geom);
 
+
+
+-- union source and target tracts
+
+DROP TABLE IF EXISTS x_source_target;
+CREATE TABLE x_source_target AS (
+    SELECT
+    x_source_ct_clipped_all.ctuid AS source_ctuid,
+    in_1956_ct.geosid AS target_ctuid,
+    ST_Intersection(ST_MakeValid(x_source_ct_clipped_all.geom),ST_MakeValid(in_1956_ct.geom)) AS geom
+    FROM x_source_ct_clipped_all LEFT JOIN in_1956_ct
+    ON ST_Intersects(ST_MakeValid(x_source_ct_clipped_all.geom),ST_MakeValid(in_1956_ct.geom))
+    WHERE x_source_ct_clipped_all.geom && in_1956_ct.geom
+);
