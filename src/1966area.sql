9ad3e444b37e75d0d7670fa6566de4e95f6f3227
Jeff Allen <jeffallen89@gmail.com>
2022-09-16 14:44:31 -0400

    clip 66 by built up

--- src/1966area.sql
+++ src/1966area.sql
@@ -3,8 +3,19 @@
 DROP INDEX IF EXISTS in_1966_ct_geom_idx; 
 CREATE INDEX in_1966_ct_geom_idx ON in_1966_ct USING GIST (geom);
 
-DROP INDEX IF EXISTS in_1971_ct_geom_idx; 
-CREATE INDEX in_1971_ct_geom_idx ON in_1971_ct USING GIST (geom);
+DROP INDEX IF EXISTS in_1971_cbf_ct_geom_idx; 
+CREATE INDEX in_1971_cbf_ct_geom_idx ON in_1971_cbf_ct USING GIST (geom);
 
 --
 
+-- clip the source CT by the built up land layer, then union out any completly clipped away, also compute area
+DROP TABLE IF EXISTS x_source_ct_clipped;
+CREATE TABLE x_source_ct_clipped AS (
+    SELECT
+    in_1966_ct.geosid AS ctuid,
+    ST_Union(ST_Intersection(ST_MakeValid(in_1966_ct.geom),ST_MakeValid(c.geom))) AS geom
+    FROM in_1966_ct LEFT JOIN (SELECT * FROM in_built_up_1971 WHERE code = 21) as c
+    ON ST_Intersects(ST_MakeValid(in_1966_ct.geom),ST_MakeValid(c.geom))
+    WHERE in_1966_ct.geom && c.geom
+    GROUP BY ctuid
+);
