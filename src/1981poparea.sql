b1604c6365d53a3b2ea8026da96cdcea790f2eaf
Jeff Allen <jeffallen89@gmail.com>
2022-10-11 16:13:39 -0400

    clip 81 ea to 91 built up

--- src/1981poparea.sql
+++ src/1981poparea.sql
@@ -22,3 +22,48 @@ CREATE TABLE pop_ct_1981 AS (
 	GROUP BY in_1981_cbf_ct.geosid 
 );
 
+
+-- get minimal table of source blocks, and add a spatial index
+DROP TABLE IF EXISTS x_source_blocks;
+CREATE TABLE x_source_blocks AS (
+    SELECT 
+    id, 
+    ctuid,
+    geom 
+    FROM in_1981_vor_ea
+    ORDER BY ctuid);
+DROP INDEX IF EXISTS x_source_blocks_geom_idx;
+CREATE INDEX x_source_blocks_geom_idx
+ON x_source_blocks
+USING GIST (geom); 
+
+
+-- clip the source blocks by the built up land layer, then union out any completly clipped away
+DROP TABLE IF EXISTS x_source_blocks_clipped;
+CREATE TABLE x_source_blocks_clipped AS (
+    SELECT
+    x_source_blocks.id,
+    x_source_blocks.ctuid,
+    ST_Union(ST_Intersection(ST_MakeValid(x_source_blocks.geom),ST_MakeValid(c.geom))) AS geom
+    FROM x_source_blocks LEFT JOIN (SELECT * FROM in_built_up_1991 WHERE code = 21) as c
+    ON ST_Intersects(ST_MakeValid(x_source_blocks.geom),ST_MakeValid(c.geom))
+    WHERE x_source_blocks.geom && c.geom
+    GROUP BY x_source_blocks.ctuid, x_source_blocks.id
+);
+
+
+DROP TABLE IF EXISTS x_source_blocks_clipped_all;
+CREATE TABLE x_source_blocks_clipped_all AS (
+    (SELECT * FROM x_source_blocks_clipped)
+    UNION ALL
+    (WITH J AS (SELECT 
+        in_1981_vor_ea.id,
+        x_source_blocks_clipped.id AS clipped_db,
+        in_1981_vor_ea.ctuid,
+        in_1981_vor_ea.geom 
+        FROM in_1981_vor_ea
+        LEFT JOIN x_source_blocks_clipped ON
+        in_1981_vor_ea.id = x_source_blocks_clipped.id
+        WHERE in_1981_vor_ea.ctuid IS NOT NULL)
+    SELECT id, ctuid, geom FROM J WHERE clipped_db IS NULL)
+);
